7bad6ddb965c4e1e6d174c4814862c0ab98da57a
Viniciusadm <viniciusam.22@gmail.com>
2021-11-15 00:10:32 -0300

    Rotas do controller people criadas

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ParticipantController;
+use App\Http\Controllers\PersonController;
 use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -33,8 +34,16 @@ Route::prefix('reports')->group(function () {
 Route::prefix('participants')->group(function () {
     Route::get('/', [ParticipantController::class, 'getAll']);
     Route::get('{id}', [ParticipantController::class, 'getById']);
-    Route::delete('{id}', [ParticipantController::class, 'deleteById']);
+    Route::delete('{id}', [ParticipantController::class, 'delete']);
     Route::post('{id}', [ParticipantController::class, 'createByIdReport']);
     
     Route::get('report/{id}', [ParticipantController::class, 'getByIdReport']);
+});
+
+Route::prefix('people')->group(function () {
+    Route::get('/', [PersonController::class, 'getAll']);
+    Route::get('{id}', [PersonController::class, 'getById']);
+    Route::post('/', [PersonController::class, 'create']);
+    Route::delete('{id}', [PersonController::class, 'delete']);
+    Route::put('{id}', [PersonController::class, 'update']);
 });
