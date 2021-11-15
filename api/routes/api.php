d89bc2a7bd2f4f9fe6a7b0b4ab7d64e1cb0d4380
Viniciusadm <viniciusam.22@gmail.com>
2021-11-14 23:32:54 -0300

    Rotas reports e participants criadas

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ParticipantController;
+use App\Http\Controllers\ReportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('reports')->group(function () {
+    Route::get('/', [ReportController::class, 'getAll']);
+    Route::get('{id}', [ReportController::class, 'getById']);
+    Route::post('/', [ReportController::class, 'create']);
+    Route::delete('{id}', [ReportController::class, 'delete']);
+    Route::patch('{id}', [ReportController::class, 'updateReport']);
+
+    Route::get('participant/{id}', [ReportController::class, 'getByParticipantId']);
+});
+
+Route::prefix('participants')->group(function () {
+    Route::get('/', [ParticipantController::class, 'getAll']);
+    Route::get('{id}', [ParticipantController::class, 'getById']);
+    Route::delete('{id}', [ParticipantController::class, 'deleteById']);
+    Route::post('{id}', [ParticipantController::class, 'createByIdReport']);
+    
+    Route::get('report/{id}', [ParticipantController::class, 'getByIdReport']);
 });
