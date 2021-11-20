944ca21d17fee4fd01c4b28fcbdd219907e8f4ef
Viniciusadm <viniciusam.22@gmail.com>
2021-11-20 09:38:53 -0300

    Downgrade de versão porque tinha baixado errado

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,15 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const routes = [
 
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
