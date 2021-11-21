37846e04e51abf985657b1a355c5e479f5a98e8b
Viniciusadm <viniciusam.22@gmail.com>
2021-11-21 15:17:20 -0300

    Edição do App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,22 @@
 <template>
-
+    <div class="app">
+        <Menu></Menu>
+        <div class="container">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+    
 </style>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+    components: {
+        Menu
+    }
+}
+</script>
