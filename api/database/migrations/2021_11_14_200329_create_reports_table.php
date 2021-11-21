a9dc171300d8a1afb787a896e468f7f873f8b419
Viniciusadm <viniciusam.22@gmail.com>
2021-11-21 15:30:58 -0300

    Adição da coluna title na tabela reports

--- api/database/migrations/2021_11_14_200329_create_reports_table.php
+++ api/database/migrations/2021_11_14_200329_create_reports_table.php
@@ -15,6 +15,7 @@ class CreateReportsTable extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->text('report');
             $table->string('humor');
             $table->enum('type', ['personal', 'daily']);
