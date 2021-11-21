725dec95bce800f4a81fa337921956a9d3236ae4
Viniciusadm <viniciusam.22@gmail.com>
2021-11-21 15:17:59 -0300

    Definição das rotas inicias

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -4,13 +4,27 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import('../views/Home.vue')
+    },
+    {
+        path: '/reports',
+        name: 'Reports',
+        component: () => import('../views/Reports.vue')
+    },
+    {
+        path: '/people',
+        name: 'People',
+        component: () => import('../views/People.vue')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
